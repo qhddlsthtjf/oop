be2ab9b564c182f5a3d77f17a69d0464eff12f75
itbank <itbank@itbank-PC>
2015-07-24 15:16:39 +0900

    2015.07.24

--- JavaOop/src/bank/Bank.java
+++ JavaOop/src/bank/Bank.java
@@ -66,18 +66,27 @@ public class Bank implements BankRole{
 	
 	@Override //계좌검색(이름) -> 리털결과 : 계좌 여러개
 	public BankBook[] searchAccountByName(String ownerName) {
-		BankBook[] accounts = null;
+		
 		//searchAccountByName() 이 메서드를 호출하면 자동으로 searchCountByName()을 먼저 호출하라 
-		int tempcount = this.searchCountByName(ownerName);
-		if (tempcount==0) {
+		int tempCount = this.searchCountByName(ownerName);
+		if (tempCount == 0) {
 			return null;
 		}
-		for (int i = 0; i < this.count; i++) {
-			if (bankBookList[i].getName().equals(ownerName)) {
-				accounts[count] = bankBookList[i];
-				count++;
-			}
+		//위처럼 필터링 하는 이유는 본 알고리즘을 타기 전에 필요없는 상태라면 알고리즘을 호출 하지 않기 위해서다.
+		//그렇지 않으면 자원(리소스 :메모리 ,DB)의 낭비를 초래한다.
+		BankBook[] accounts = new BankBook[tempCount];
+		tempCount = 0; //0으로 초기화 시켜서 배열의 인덱스로 사용해야함.
+		
+			for (int i = 0; i < accounts.length; i++) {
+				if (bankBookList[i].getName().equals(ownerName)) {
+					
+					accounts[tempCount] = bankBookList[i];
+					
+					tempCount++;
+				}
 		}
+		
+		
 		return accounts;
 	}
 	 
@@ -87,8 +96,9 @@ public class Bank implements BankRole{
 		 for 문을 돌면서 파라미터로 받은 이름과 동일한 통장(계좌)가 있다면 count 를 0에서 1씩 증가시켜라
 		 * */
 		int count = 0;
-		for (int i = 0; i < bankBookList.length; i++) {//bankBookList.length<<는 this.count 랑 같다 
+		for (int i = 0; i < this.getCount(); i++) {//bankBookList.length<<는 this.count 랑 같다 
 			if (bankBookList[i].getName().equals(name)) {
+				
 				count++;
 			}
 		}
@@ -98,12 +108,18 @@ public class Bank implements BankRole{
 	@Override //계좌해지 -> 리턴결과 : true or false
 	public boolean closeAccount(String accountNo) {
 		//flag 은 삭제가 성공적으로 이루어 지면 true 를 리턴하고 삭제할게 없으면 false 리턴 
-		boolean flag = false;
+		boolean closeOk = false;
+		BankBook bankBook = this.searchAccountByAccountNo(accountNo);
+		//필터링에서는 if -else 구문을 사용하지 않고 if 문을 사용한다.
+		if (bankBook == null) {
+			System.out.println("해당 계좌가 존재하지 않습니다.");
+			return closeOk;
+		}
 		//String(문자열) 로 들어온 값을 숫자로 바꿔서 비교 
 		int searchAccountNO = Integer.parseInt(accountNo);
 		for (int i = 0; i < this.count; i++) {
 			if (bankBookList[i].getBankbookNo()==searchAccountNO) {
-				flag = true;
+				
 				/*
 				 j=i로 바꾼이유는 홍길동의 계좌가 은행 전체 계좌의 50번째 라면 내부 for문에서 다시 처음0부터 회전하지않고 
 				 홍길동의 계좌가 있는 인덱스 번호부터 뒤에 있는 계좌번호를 덮어 쓰는 방식으로 진행한후 맨 마지막 인덱스번호를 제거하면
@@ -114,10 +130,12 @@ public class Bank implements BankRole{
 					
 				}
 				count--;
+				//위 알고르짐을 거친 후에야 계좌 삭제가 일어났다고 본다.
+				closeOk = true; 
 			}
 		}
 	
-		return flag;
+		return closeOk;
 	}
 	
 
--- JavaOop/src/bank/KBbank.java
+++ JavaOop/src/bank/KBbank.java
@@ -22,13 +22,27 @@ public class KBbank {
 		BankBook[] searchArr = new BankBook[searchCount];
 		searchArr = bank.searchAccountByName(searchName);
 		for (BankBook bankBook : searchArr) {
-			 bankBook.showAccount();
+			System.out.println(bankBook.showAccount());
+			
 			
 		}
 		System.out.println("검색하려는 계좌번호 ?");
-		
-		bank.searchAccountByAccountNo(scanner.next()).showAccount();
-		
+		//객체가 메소드를 호출한 후 연산을 통해 결과를 리턴 하였지만 그 리턴값을 받는 변수터리는 개발자가 별도로 해줘야 한다.
+		//그 방법은 메소드가 리턴하는 리턴타입을 알아내서 그타입으로 변수하나를 만든 다음 그 변수에 리턴값을 할당한다.
+		BankBook bankBook = bank.searchAccountByAccountNo(scanner.next());
+		System.out.println(bankBook.showAccount());
+		System.out.println("삭제하려는 계좌번호?");//"1234"와 1234 는 비교가 불가
+		//int 와 String 의 값을 서로 비교하려면 
+		//String 타입을 int 로 바꿔줘야 한다.
+		//int one = String.valueOf("1234"); 의 결과값은 1234가 들어간다.
+		//String delet = scanner.next();
+		//String deleteAccount = String.valueOf(delet);
+		boolean closeOk= bank.closeAccount(scanner.next());
+		if (closeOk==true) {
+			System.out.println("삭제되었습니다.");
+		} else {
+			System.out.println("해당 계좌가 없습니다."); 
+		}
 		
 	}
 }
